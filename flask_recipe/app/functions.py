db24705620966f3a3f8bf7595efef08faaea799c
ewartj <ewart.jonny@gmail.com>
2023-08-30 21:09:36 +0100

    improved structure with an app folder

---
+++ flask_recipe/app/functions.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, redirect, url_for
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
+from app.form import RecipeForm
+from initial_test import Recipes, check_if_ingredient
+from constants import const
+import pandas as pd
+import os
+import requests
+from foodbert.food_extractor.food_model import FoodModel
+from constants import const
+
+appid = const["appid"]
+api_key = const["api_key"]
+
+def get_ingredients(food):
+    ingredients = []
+    for i in food:
+        ingredient_tag = len(i['Ingredient'])
+        if ingredient_tag > 0:
+            print(ingredient_tag)
+            for j, k in enumerate(i['Ingredient']):
+                ingredients.append(i['Ingredient'][j]["text"])
+    return ingredients
+
+def query_food_api(query, appid, api_key):
+    query = str(query)
+    r = requests.get(
+        'https://api.edamam.com/api/recipes/v2',
+        headers={'Accept': 'application/json'},
+        params={
+            'app_id': appid,
+            'app_key': api_key,
+            'type': 'any',
+            'q': query
+        })
+    data = r.json()
+    return data
+
+def get_recipe_info(data):
+    recipes_dict = {}
+    total_matches = data["count"]
+    for i in range(0, total_matches):
+        recipes_dict[data["hits"][0]['recipe']["label"]] = {
+            "source":data["hits"][0]['recipe']["source"],
+            "url":data["hits"][0]['recipe']["url"],
+            "cautions":data["hits"][0]['recipe']["cautions"]
+        }
+    return recipes_dict
+
+
+def split(txt, seps):
+    # https://stackoverflow.com/questions/4697006/python-split-string-by-list-of-separators
+    default_sep = seps[0]
+
+    # we skip seps[0] because that's the default separator
+    for sep in seps[1:]:
+        txt = txt.replace(sep, default_sep)
+    return [i.strip() for i in txt.split(default_sep)]
--- flask_recipe/app/routes.py
+++ flask_recipe/app/routes.py
@@ -1,7 +1,9 @@
+from app import app
+
 from flask import Flask, render_template, request, redirect, url_for
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
-from form import RecipeForm
+from app.form import RecipeForm
 from initial_test import Recipes, check_if_ingredient
 from constants import const
 import pandas as pd
@@ -9,17 +11,18 @@ import os
 import requests
 from foodbert.food_extractor.food_model import FoodModel
 from constants import const
+from app.form import RecipeForm
+from app.functions import (get_ingredients, get_recipe_info, 
+                            query_food_api, split)
 model = FoodModel("chambliss/distilbert-for-food-extraction")
 
-bot = ChatBot('Friend') #create the bot
+appid = const["appid"]
+api_key = const["api_key"]
+
+bot = ChatBot('Friend')
 
 trainer = ListTrainer(bot)
-
-#bot.train(conv) # teacher train the bot
-
-for knowledeg in os.listdir('base'):
-	BotMemory = open('base/'+ knowledeg, 'r').readlines()
-	trainer.train([
+trainer.train([
 'Hi. Do you need help to find a recipe?',
 'Yes',
 'Okay what do you have?',
@@ -27,17 +30,9 @@ for knowledeg in os.listdir('base'):
 'Would you like a chat',
 "Yes"
 ])
-    
-
-
-appid = const["appid"]
-api_key = const["api_key"]
-
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 @app.route('/')
+@app.route('/index')
 def index():
 	return render_template('index.html')
 
@@ -96,71 +91,3 @@ def result_db():
     output_data = pd.read_csv("db.csv")
     output_json = output_data.to_json(orient='table',index=False)
     return output_json
-
-# bot_full = ChatBot('Full')
-# trainer = ChatterBotCorpusTrainer(bot_full)
-# trainer.train("chatterbot.corpus.english")
-
-# @app.route('/full')
-# def full():
-# 	return render_template('full.html')
-
-# @app.route('/process_full',methods=['POST'])
-# def process_full():
-#     user_input=request.form['user_input']
-#     print(user_input)
-#     bot_response=bot_full.get_response(user_input)
-#     bot_response=str(bot_response)
-#     print("Friend: "+bot_response)
-#     return render_template('full.html',user_input=user_input,
-# 		bot_response=bot_response
-# 		)
-
-def get_ingredients(food):
-    ingredients = []
-    for i in food:
-        ingredient_tag = len(i['Ingredient'])
-        if ingredient_tag > 0:
-            print(ingredient_tag)
-            for j, k in enumerate(i['Ingredient']):
-                ingredients.append(i['Ingredient'][j]["text"])
-    return ingredients
-
-def query_food_api(query, appid, api_key):
-    query = str(query)
-    r = requests.get(
-        'https://api.edamam.com/api/recipes/v2',
-        headers={'Accept': 'application/json'},
-        params={
-            'app_id': appid,
-            'app_key': api_key,
-            'type': 'any',
-            'q': query
-        })
-    data = r.json()
-    return data
-
-def get_recipe_info(data):
-    recipes_dict = {}
-    total_matches = data["count"]
-    for i in range(0, total_matches):
-        recipes_dict[data["hits"][0]['recipe']["label"]] = {
-            "source":data["hits"][0]['recipe']["source"],
-            "url":data["hits"][0]['recipe']["url"],
-            "cautions":data["hits"][0]['recipe']["cautions"]
-        }
-    return recipes_dict
-
-
-def split(txt, seps):
-    # https://stackoverflow.com/questions/4697006/python-split-string-by-list-of-separators
-    default_sep = seps[0]
-
-    # we skip seps[0] because that's the default separator
-    for sep in seps[1:]:
-        txt = txt.replace(sep, default_sep)
-    return [i.strip() for i in txt.split(default_sep)]
-
-
-if __name__=='__main__':
-	app.run()
