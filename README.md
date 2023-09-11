9d715b9a660b7fe0fc38f50bf5028e781d26f326
ewart.jonny@gmail.com <ewart.jonny@gmail.com>
2023-09-11 21:11:14 +0100

    removed excess files

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# food_chatbot
-Chatbot made as dissertation for Sunderland UNiversity Dissertation
+# Flask_recipe
+
+This is a flask app designed to query the recipe database ().
+
+A basic chatbot can be used but `Search` can be used to navigate directly to it.
+
+## Before using
+
+Foodbert needs to be cloned into the flask_recipe/ folder and installed. It can be found in this [repo](https://github.com/chambliss/foodbert) and was  made by [chambliss](https://github.com/chambliss). The author of flask_recipe has made no changes to the code.
+
+To install:
+```console
+git clone git@github.com:chambliss/foodbert.git
+cd foodbert
+pip install -e .
+```
--- api_intro/main.py
+++ api_intro/main.py
@@ -2,18 +2,23 @@ from fastapi import FastAPI
 import uvicorn
 import os
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
 
 app = FastAPI()
 
-english_bot = ChatBot("Chatterbot")
+bot = ChatBot('Query')
 
-trainer = ChatterBotCorpusTrainer(english_bot)
-trainer.train("chatterbot.corpus.english")
+trainer = ListTrainer(bot)
+trainer.train([
+'Hi. Do you need help to find a recipe?',
+'Yes',
+'Would you like a chat',
+"Yes"
+])
 
 @app.get("/chatbot/{query}")
 async def input(query: str):
-    return str(english_bot.get_response(query))
+    return str(bot.get_response(query))
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=int(os.getenv('APP_PORT')), host='0.0.0.0')
